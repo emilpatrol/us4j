d74e985ffa346ae3002709c55f6f8f0d43232670
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-14 05:39:51 +0000

    Deploy emilpatrol/us4j to github.com/emilpatrol/us4j.git:gh-pages

---
+++ debug/deps/us4j-69af60d558a08107.d
@@ -0,0 +1,5 @@
+/home/travis/build/emilpatrol/us4j/target/debug/deps/us4j-69af60d558a08107.rmeta: src/main.rs
+
+/home/travis/build/emilpatrol/us4j/target/debug/deps/us4j-69af60d558a08107.d: src/main.rs
+
+src/main.rs:
---
+++ debug/deps/us4j-da034129f77dfa04.d
@@ -0,0 +1,5 @@
+/home/travis/build/emilpatrol/us4j/target/debug/deps/us4j-da034129f77dfa04: src/main.rs
+
+/home/travis/build/emilpatrol/us4j/target/debug/deps/us4j-da034129f77dfa04.d: src/main.rs
+
+src/main.rs:
